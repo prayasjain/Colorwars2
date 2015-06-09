c83187c3429aeddbae9ee3fbd9af8e3ea88ad633
prayasjain <prayas.jain.cse14@iitbhu.ac.in>
2015-06-10 01:38:26 +0530

    animation
    frontpage

--- gen/com/example/colorwars/R.java
+++ gen/com/example/colorwars/R.java
@@ -10,6 +10,9 @@ package com.example.colorwars;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int mainscreen=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,8 +21,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int aboutus=0x7f020000;
@@ -39,122 +42,128 @@ public final class R {
         public static final int blues=0x7f02000e;
         public static final int blueyellow=0x7f02000f;
         public static final int bnew=0x7f020010;
-        public static final int cw=0x7f020011;
-        public static final int download=0x7f020012;
-        public static final int gnew=0x7f020013;
-        public static final int green=0x7f020014;
-        public static final int greens=0x7f020015;
-        public static final int grey=0x7f020016;
-        public static final int ic_launcher=0x7f020017;
-        public static final int ic_launcher2=0x7f020018;
-        public static final int ic_launcher3=0x7f020019;
-        public static final int play=0x7f02001a;
-        public static final int purple=0x7f02001b;
-        public static final int question=0x7f02001c;
-        public static final int red=0x7f02001d;
-        public static final int redgreen=0x7f02001e;
-        public static final int reds=0x7f02001f;
-        public static final int redyellow=0x7f020020;
-        public static final int rnew=0x7f020021;
-        public static final int settings=0x7f020022;
-        public static final int white=0x7f020023;
-        public static final int white2=0x7f020024;
-        public static final int white2s=0x7f020025;
-        public static final int yellow=0x7f020026;
-        public static final int yellowgreen=0x7f020027;
-        public static final int yellows=0x7f020028;
-        public static final int ynew=0x7f020029;
+        public static final int buttontouch=0x7f020011;
+        public static final int contactbutton=0x7f020012;
+        public static final int contactshape=0x7f020013;
+        public static final int cw=0x7f020014;
+        public static final int download=0x7f020015;
+        public static final int gnew=0x7f020016;
+        public static final int green=0x7f020017;
+        public static final int greens=0x7f020018;
+        public static final int grey=0x7f020019;
+        public static final int helpbutton=0x7f02001a;
+        public static final int helpshape=0x7f02001b;
+        public static final int ic_launcher=0x7f02001c;
+        public static final int ic_launcher2=0x7f02001d;
+        public static final int ic_launcher3=0x7f02001e;
+        public static final int play=0x7f02001f;
+        public static final int playbutton=0x7f020020;
+        public static final int playshape=0x7f020021;
+        public static final int purple=0x7f020022;
+        public static final int question=0x7f020023;
+        public static final int red=0x7f020024;
+        public static final int redgreen=0x7f020025;
+        public static final int reds=0x7f020026;
+        public static final int redyellow=0x7f020027;
+        public static final int rnew=0x7f020028;
+        public static final int settings=0x7f020029;
+        public static final int white=0x7f02002a;
+        public static final int white2=0x7f02002b;
+        public static final int white2s=0x7f02002c;
+        public static final int yellow=0x7f02002d;
+        public static final int yellowgreen=0x7f02002e;
+        public static final int yellows=0x7f02002f;
+        public static final int ynew=0x7f020030;
     }
     public static final class id {
-        public static final int Bottom=0x7f08003c;
-        public static final int Button01=0x7f080006;
-        public static final int Button02=0x7f080007;
-        public static final int Button03=0x7f080015;
-        public static final int Button04=0x7f080012;
-        public static final int Button05=0x7f080011;
-        public static final int Button06=0x7f080018;
-        public static final int Button07=0x7f080019;
-        public static final int Button08=0x7f08001a;
-        public static final int Button09=0x7f080013;
-        public static final int Button10=0x7f080014;
-        public static final int Button11=0x7f080016;
-        public static final int Button12=0x7f08001b;
-        public static final int Button13=0x7f080017;
-        public static final int Button14=0x7f08001e;
-        public static final int Button15=0x7f080021;
-        public static final int Button16=0x7f08001f;
-        public static final int Button17=0x7f08001c;
-        public static final int Button18=0x7f080020;
-        public static final int Button19=0x7f08001d;
-        public static final int Button20=0x7f080023;
-        public static final int Button21=0x7f080026;
-        public static final int Button22=0x7f080024;
-        public static final int Button23=0x7f080025;
-        public static final int Button24=0x7f080022;
-        public static final int Col1=0x7f080052;
-        public static final int Col2=0x7f080053;
-        public static final int Col3=0x7f080054;
-        public static final int Col4=0x7f080055;
-        public static final int Left=0x7f08003f;
-        public static final int LinearLayout01=0x7f08000c;
-        public static final int MoveLimit=0x7f080051;
-        public static final int Right=0x7f08003e;
-        public static final int TextView01=0x7f080008;
-        public static final int TextView02=0x7f080005;
-        public static final int TimeLimit=0x7f080050;
-        public static final int Top=0x7f08003d;
-        public static final int action_settings=0x7f080058;
-        public static final int bluemoves=0x7f080034;
-        public static final int bluepower1=0x7f08002f;
-        public static final int bluepower2=0x7f08002e;
-        public static final int bluepower3=0x7f08002d;
-        public static final int bscore=0x7f080033;
-        public static final int button1=0x7f080009;
-        public static final int button10=0x7f08004d;
-        public static final int button11=0x7f08004e;
-        public static final int button2=0x7f08000b;
-        public static final int button25=0x7f080042;
-        public static final int button26=0x7f080043;
-        public static final int button3=0x7f08000d;
-        public static final int button4=0x7f08000e;
-        public static final int button5=0x7f080049;
-        public static final int button6=0x7f08004a;
-        public static final int button7=0x7f08004b;
-        public static final int button8=0x7f08004c;
-        public static final int button9=0x7f080040;
-        public static final int checkBox1=0x7f080056;
-        public static final int layout5=0x7f080010;
-        public static final int layoutp2=0x7f08002c;
-        public static final int linearLayout1=0x7f08000a;
-        public static final int linearLayout2=0x7f080027;
-        public static final int linearLayout3=0x7f080035;
-        public static final int linearLayout4=0x7f080039;
-        public static final int mode=0x7f08004f;
-        public static final int play=0x7f080057;
-        public static final int playview=0x7f08000f;
-        public static final int redmoves=0x7f080031;
-        public static final int redpower1=0x7f080029;
-        public static final int redpower2=0x7f08002a;
-        public static final int redpower3=0x7f08002b;
-        public static final int rlcomp=0x7f080045;
-        public static final int rlplayer=0x7f080047;
-        public static final int rscore=0x7f080032;
-        public static final int textView1=0x7f080002;
-        public static final int textView11=0x7f080041;
-        public static final int textView12=0x7f08003a;
-        public static final int textView13=0x7f08003b;
-        public static final int textView14=0x7f080036;
-        public static final int textView15=0x7f080037;
-        public static final int textView16=0x7f080038;
-        public static final int textView2=0x7f080000;
-        public static final int textView3=0x7f080001;
-        public static final int textView4=0x7f080003;
-        public static final int textView5=0x7f080004;
-        public static final int textView6=0x7f080030;
-        public static final int textView7=0x7f080028;
-        public static final int turn=0x7f080044;
-        public static final int tvdebug=0x7f080046;
-        public static final int tvdebug2=0x7f080048;
+        public static final int Bottom=0x7f09003b;
+        public static final int Button01=0x7f090008;
+        public static final int Button02=0x7f090006;
+        public static final int Button03=0x7f090014;
+        public static final int Button04=0x7f090011;
+        public static final int Button05=0x7f090010;
+        public static final int Button06=0x7f090017;
+        public static final int Button07=0x7f090018;
+        public static final int Button08=0x7f090019;
+        public static final int Button09=0x7f090012;
+        public static final int Button10=0x7f090013;
+        public static final int Button11=0x7f090015;
+        public static final int Button12=0x7f09001a;
+        public static final int Button13=0x7f090016;
+        public static final int Button14=0x7f09001d;
+        public static final int Button15=0x7f090020;
+        public static final int Button16=0x7f09001e;
+        public static final int Button17=0x7f09001b;
+        public static final int Button18=0x7f09001f;
+        public static final int Button19=0x7f09001c;
+        public static final int Button20=0x7f090022;
+        public static final int Button21=0x7f090025;
+        public static final int Button22=0x7f090023;
+        public static final int Button23=0x7f090024;
+        public static final int Button24=0x7f090021;
+        public static final int Col1=0x7f090051;
+        public static final int Col2=0x7f090052;
+        public static final int Col3=0x7f090053;
+        public static final int Col4=0x7f090054;
+        public static final int Left=0x7f09003e;
+        public static final int LinearLayout01=0x7f09000b;
+        public static final int MoveLimit=0x7f090050;
+        public static final int Right=0x7f09003d;
+        public static final int TimeLimit=0x7f09004f;
+        public static final int Top=0x7f09003c;
+        public static final int action_settings=0x7f090057;
+        public static final int bluemoves=0x7f090033;
+        public static final int bluepower1=0x7f09002e;
+        public static final int bluepower2=0x7f09002d;
+        public static final int bluepower3=0x7f09002c;
+        public static final int bscore=0x7f090032;
+        public static final int button1=0x7f090007;
+        public static final int button10=0x7f09004c;
+        public static final int button11=0x7f09004d;
+        public static final int button2=0x7f09000a;
+        public static final int button25=0x7f090041;
+        public static final int button26=0x7f090042;
+        public static final int button3=0x7f09000c;
+        public static final int button4=0x7f09000d;
+        public static final int button5=0x7f090048;
+        public static final int button6=0x7f090049;
+        public static final int button7=0x7f09004a;
+        public static final int button8=0x7f09004b;
+        public static final int button9=0x7f09003f;
+        public static final int checkBox1=0x7f090055;
+        public static final int layout5=0x7f09000f;
+        public static final int layoutp2=0x7f09002b;
+        public static final int linearLayout1=0x7f090009;
+        public static final int linearLayout2=0x7f090026;
+        public static final int linearLayout3=0x7f090034;
+        public static final int linearLayout4=0x7f090038;
+        public static final int mode=0x7f09004e;
+        public static final int play=0x7f090056;
+        public static final int playview=0x7f09000e;
+        public static final int redmoves=0x7f090030;
+        public static final int redpower1=0x7f090028;
+        public static final int redpower2=0x7f090029;
+        public static final int redpower3=0x7f09002a;
+        public static final int rlcomp=0x7f090044;
+        public static final int rlplayer=0x7f090046;
+        public static final int rscore=0x7f090031;
+        public static final int textView1=0x7f090002;
+        public static final int textView11=0x7f090040;
+        public static final int textView12=0x7f090039;
+        public static final int textView13=0x7f09003a;
+        public static final int textView14=0x7f090035;
+        public static final int textView15=0x7f090036;
+        public static final int textView16=0x7f090037;
+        public static final int textView2=0x7f090000;
+        public static final int textView3=0x7f090001;
+        public static final int textView4=0x7f090003;
+        public static final int textView5=0x7f090004;
+        public static final int textView6=0x7f09002f;
+        public static final int textView7=0x7f090027;
+        public static final int turn=0x7f090043;
+        public static final int tvcw=0x7f090005;
+        public static final int tvdebug=0x7f090045;
+        public static final int tvdebug2=0x7f090047;
     }
     public static final class layout {
         public static final int aboutus=0x7f030000;
@@ -167,27 +176,27 @@ public final class R {
         public static final int win=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int AboutUs=0x7f050005;
-        public static final int BallColor=0x7f05000c;
-        public static final int Bottom=0x7f05000f;
-        public static final int C1=0x7f050006;
-        public static final int C2=0x7f050007;
-        public static final int C3=0x7f050008;
-        public static final int C4=0x7f050009;
-        public static final int GameMode=0x7f05000d;
-        public static final int Help=0x7f050004;
-        public static final int Left=0x7f05000e;
-        public static final int Moves=0x7f05000b;
-        public static final int Play=0x7f050003;
-        public static final int Right=0x7f050011;
-        public static final int Time=0x7f05000a;
-        public static final int Top=0x7f050010;
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int AboutUs=0x7f060005;
+        public static final int BallColor=0x7f06000c;
+        public static final int Bottom=0x7f06000f;
+        public static final int C1=0x7f060006;
+        public static final int C2=0x7f060007;
+        public static final int C3=0x7f060008;
+        public static final int C4=0x7f060009;
+        public static final int GameMode=0x7f06000d;
+        public static final int Help=0x7f060004;
+        public static final int Left=0x7f06000e;
+        public static final int Moves=0x7f06000b;
+        public static final int Play=0x7f060003;
+        public static final int Right=0x7f060011;
+        public static final int Time=0x7f06000a;
+        public static final int Top=0x7f060010;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -211,10 +220,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/colorwars/MainActivity.java
+++ src/com/example/colorwars/MainActivity.java
@@ -1,66 +1,175 @@
 package com.example.colorwars;
 
+import java.io.IOException;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
- Button Play,About,Help;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Play=(Button)findViewById(R.id.Button02);
-        Help=(Button)findViewById(R.id.button1);
-        About=(Button)findViewById(R.id.Button01);
-        Play.setOnClickListener(this);
-        Help.setOnClickListener(this);
-        About.setOnClickListener(this);
-    }
+	Button Play, About, Help;
+	Typeface font;
+	TextView cw;
+	ViewGroup ourView;
+	float pos ;
+	long tdiff ;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		ourView = (ViewGroup) inflater.inflate(R.layout.activity_main, null);
 
+		setContentView(ourView);
+
+		Play = (Button) findViewById(R.id.Button02);
+		Help = (Button) findViewById(R.id.button1);
+		About = (Button) findViewById(R.id.Button01);
+		Play.setOnClickListener(this);
+		Help.setOnClickListener(this);
+		About.setOnClickListener(this);
+		cw = (TextView) findViewById(R.id.tvcw);
+		Context context = MainActivity.this;
+		font = Typeface.createFromAsset(context.getAssets(), "font1.ttf");
+		cw.setTypeface(font);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	@Override
 	public void onClick(View arg0) {
-		switch(arg0.getId()){
-		
-		case R.id.Button01: Intent About= new Intent(MainActivity.this,AboutActivity.class);
-		MainActivity.this.startActivity(About);
-			break;                    
-		case R.id.Button02: Intent Settings = new Intent(MainActivity.this,settings2Activity.class);
-		                    MainActivity.this.startActivity(Settings);
-		                    break;
-		case R.id.button1 : Intent Help = new Intent(MainActivity.this,HelpActivity.class) ;
-							MainActivity.this.startActivity(Help) ;
-							break ;
+
+		switch (arg0.getId()) {
+
+		case R.id.Button01:
+			movebuttons(3);
+
+			Thread timer2 = new Thread() {
+				public void run() {
+					try {
+
+						sleep(tdiff+250);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} finally {
+						Intent About = new Intent(MainActivity.this,
+								AboutActivity.class);
+						startActivity(About);
+					}
+				}
+			};
+			timer2.start();
+
+			break;
+		case R.id.Button02:
+			movebuttons(1);
+			Thread timer = new Thread() {
+				public void run() {
+					try {
+
+						sleep(tdiff+250);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} finally {
+						Intent open = new Intent(MainActivity.this,
+								settings2Activity.class);
+						startActivity(open);
+					}
+				}
+			};
+			timer.start();
+			break;
+		case R.id.button1:
+			movebuttons(2);
+
+			Thread timer3 = new Thread() {
+				public void run() {
+					try {
+
+						sleep(tdiff+250);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} finally {
+						Intent Help = new Intent(MainActivity.this,
+								HelpActivity.class);
+						startActivity(Help);
+					}
+				}
+			};
+			timer3.start();
+			break;
 		}
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	private void movebuttons(int type) {
+		// TODO Auto-generated method stub
+		long time = SystemClock.currentThreadTimeMillis();
+		if (type == 1) {
+			
+			for (int i = 0; i < 600; i++) {
+				Play.animate().xBy(i);
+				About.animate().xBy(-i);
+				Help.animate().xBy(-i);
+				cw.animate().xBy(-i);
+				
+				
+			}
+
+		} else if (type == 3) {
+			for (int i = 0; i < 600; i++) {
+				Play.animate().xBy(-i);
+				About.animate().xBy(i);
+				Help.animate().xBy(-i);
+				cw.animate().xBy(-i);
+			}
+		} else if (type == 2) {
+			for (int i = 0; i < 600; i++) {
+				Play.animate().xBy(-i);
+				About.animate().xBy(-i);
+				Help.animate().xBy(i);
+				cw.animate().xBy(-i);
+			}
+		}
+		tdiff=SystemClock.currentThreadTimeMillis()-time ;
 	}
 }
